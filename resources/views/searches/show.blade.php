6bd0b3831fb1f2f4a9cbb2520bd04606bfa3bcd2
Nikolay <nikolay.vanzhin@yandex.ru>
2022-04-12 22:15:57 +0500

    search.show js updated

--- resources/views/searches/show.blade.php
+++ resources/views/searches/show.blade.php
@@ -142,11 +142,11 @@
 
         </div>
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="saveSearchModal" tabindex="-1" aria-labelledby="saveSearchModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Сохранение поиска</h5>
+                        <h5 class="modal-title" id="saveSearchModalLabel">Сохранение поиска</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <form class="was-validated" method="post" action="{{ route('search.update', [$search]) }}">
